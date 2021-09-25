804acfd40f5df63768dde946160f416833be2a85
Abraham MAHASONGONA <abraham.mahasongona@gmail.com>
2021-09-25 14:05:28 +0300

    First animation

--- utils/domListener.js
+++ utils/domListener.js
@@ -15,5 +15,16 @@ window.onscroll = function () {
     } else if (isScrolledIntoView(slide2)) {
         slide2.classList.add('noOp');
         document.querySelector('.text-left-1').classList.add('animeToShow');
+        document.querySelector('.text-left-2').classList.add('animeToShow');
+        document.querySelector('.text-left-3').classList.add('animeToShow');
+        document.querySelector('.text-left-4').classList.add('animeToShow');
+        document.querySelector('.text-left-5').classList.add('animeToShow');
+        document.querySelector('.text-left-6').classList.add('animeToShow');
+        document.querySelector('.text-right-1').classList.add('animeToShow');
+        document.querySelector('.text-right-2').classList.add('animeToShow');
+        document.querySelector('.text-right-3').classList.add('animeToShow');
+        document.querySelector('.text-right-4').classList.add('animeToShow');
+        document.querySelector('.text-right-5').classList.add('animeToShow');
+        document.querySelector('.text-right-6').classList.add('animeToShow');
     }
 }
